4d77acfa369e3ad24f6c3cd485c4621be7df2e13
pranjol11 <pranjoldas2004@gmail.com>
2026-04-08 17:27:24 -0500

    Update packages, add color constant file

---
+++ utils/colors.ts
@@ -0,0 +1,13 @@
+// Color constants
+export const COLORS = {
+  // Primary
+  black: '#111',
+  white: 'white',
+
+  // Grays
+  darkGray: '#666',
+  lightGray: '#f2f2f2',
+
+  // Shadows
+  shadowDark: '#000',
+} as const;
